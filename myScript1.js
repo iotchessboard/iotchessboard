355304da25af294734ac766f9e1e6d43a2fb1304
andinel <andine.luick@gmail.com>
2018-06-01 13:46:35 +0200

    a

--- myScript1.js
+++
@@ -1,9 +0,0 @@
-<h1>Test javascript</h1>
-<p id="demo">A Paragraph</p>
-<button type="button" onclick="myFunction()">Try it</button>
-
-<script>
-function myFunction() {
-   document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
-</script>
